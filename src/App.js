bb8cc26a05411f31c64b4b14c5f2ce0946215e80
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-15 22:56:41 +1100

    weather outputs in json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import './css/main.css';
+
+
+let country = "";
+let city = "";
+
+const displayWeather = () => {
+
+}
+
 
 const apiKey = {
   key: "c65ae928fe770da8a48731205f698b15",
@@ -20,7 +29,51 @@ function dateBuilder(d) {
 
 }
 
+
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  let [country, setCountry] = useState(null);
+  let [city, setCity] = useState(null);
+
+
+
+  function funcSetCountry(country) {
+    setCountry(country.target.value);
+    console.log(country);
+  }
+
+  function funcSetCity(city) {
+    setCity(city.target.value);
+    console.log("City: " + city);
+  }
+
+  const findWeather = () => {
+
+    let lookup = require('country-code-lookup');
+
+    console.log("City: " + city);
+    console.log("Country: " + country);
+
+    country = lookup.byCountry(country).iso2;
+
+    console.log("Country: " + country);
+
+    if ((city !== "" || city !== null) && (country !== "" || country !== null)) {
+      fetch(`${apiKey.link}weather?q=${city},${country}&units=metric&appid=${apiKey.key}`)
+      .then(res => res.json())
+      .then(result => { 
+        displayWeather(result);
+        setQuery('');
+        console.log(result);
+      });
+    }
+
+
+  }
+
   return (
     <div className="main">
       <div id="searchForm">
@@ -28,13 +81,20 @@ function App() {
         <input
           type="text"
           required
+          onChange={funcSetCountry}
         />
         <h3>City name</h3>
         <input
           type="text"
           required
+          onChange={funcSetCity}
         />
-
+        <br />
+        <button onClick={findWeather}>
+          Search
+        </button>
+      </div>
+      <div id="output">
 
       </div>
     </div>
